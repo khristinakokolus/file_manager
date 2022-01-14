dc0cd826f06bd8f1ce66d49d74844e775833cf44
khristinakokolus <kokolussska@gmail.com>
2022-01-14 20:01:02 +0200

    fixed archivation

---
+++ b/filemanager_autogen/DIG64O274K/moc_filemanager.cpp
@@ -0,0 +1,214 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'filemanager.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../inc/filemanager.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'filemanager.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_FileManager_t {
+    QByteArrayData data[30];
+    char stringdata0[340];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_FileManager_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_FileManager_t qt_meta_stringdata_FileManager = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "FileManager"
+QT_MOC_LITERAL(1, 12, 12), // "clickedFirst"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 11), // "QModelIndex"
+QT_MOC_LITERAL(4, 38, 5), // "index"
+QT_MOC_LITERAL(5, 44, 13), // "clickedSecond"
+QT_MOC_LITERAL(6, 58, 10), // "deleteItem"
+QT_MOC_LITERAL(7, 69, 12), // "copyItemFrom"
+QT_MOC_LITERAL(8, 82, 12), // "copyFromPath"
+QT_MOC_LITERAL(9, 95, 10), // "copyItemTo"
+QT_MOC_LITERAL(10, 106, 10), // "copyToPath"
+QT_MOC_LITERAL(11, 117, 8), // "openFile"
+QT_MOC_LITERAL(12, 126, 11), // "openHexFile"
+QT_MOC_LITERAL(13, 138, 10), // "renameItem"
+QT_MOC_LITERAL(14, 149, 10), // "runProgram"
+QT_MOC_LITERAL(15, 160, 11), // "searchFiles"
+QT_MOC_LITERAL(16, 172, 15), // "createDirectory"
+QT_MOC_LITERAL(17, 188, 10), // "createFile"
+QT_MOC_LITERAL(18, 199, 4), // "help"
+QT_MOC_LITERAL(19, 204, 10), // "unzipFiles"
+QT_MOC_LITERAL(20, 215, 8), // "zipFiles"
+QT_MOC_LITERAL(21, 224, 11), // "listArchive"
+QT_MOC_LITERAL(22, 236, 13), // "windowMessage"
+QT_MOC_LITERAL(23, 250, 6), // "QFile&"
+QT_MOC_LITERAL(24, 257, 4), // "file"
+QT_MOC_LITERAL(25, 262, 11), // "const char*"
+QT_MOC_LITERAL(26, 274, 4), // "name"
+QT_MOC_LITERAL(27, 279, 4), // "text"
+QT_MOC_LITERAL(28, 284, 26), // "on_tableView_doubleClicked"
+QT_MOC_LITERAL(29, 311, 28) // "on_tableView_2_doubleClicked"
+
+    },
+    "FileManager\0clickedFirst\0\0QModelIndex\0"
+    "index\0clickedSecond\0deleteItem\0"
+    "copyItemFrom\0copyFromPath\0copyItemTo\0"
+    "copyToPath\0openFile\0openHexFile\0"
+    "renameItem\0runProgram\0searchFiles\0"
+    "createDirectory\0createFile\0help\0"
+    "unzipFiles\0zipFiles\0listArchive\0"
+    "windowMessage\0QFile&\0file\0const char*\0"
+    "name\0text\0on_tableView_doubleClicked\0"
+    "on_tableView_2_doubleClicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_FileManager[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  119,    2, 0x08 /* Private */,
+       5,    1,  122,    2, 0x08 /* Private */,
+       6,    0,  125,    2, 0x08 /* Private */,
+       7,    1,  126,    2, 0x08 /* Private */,
+       7,    0,  129,    2, 0x28 /* Private | MethodCloned */,
+       9,    1,  130,    2, 0x08 /* Private */,
+       9,    0,  133,    2, 0x28 /* Private | MethodCloned */,
+      11,    0,  134,    2, 0x08 /* Private */,
+      12,    0,  135,    2, 0x08 /* Private */,
+      13,    0,  136,    2, 0x08 /* Private */,
+      14,    0,  137,    2, 0x08 /* Private */,
+      15,    0,  138,    2, 0x08 /* Private */,
+      16,    0,  139,    2, 0x08 /* Private */,
+      17,    0,  140,    2, 0x08 /* Private */,
+      18,    0,  141,    2, 0x08 /* Private */,
+      19,    0,  142,    2, 0x08 /* Private */,
+      20,    0,  143,    2, 0x08 /* Private */,
+      21,    0,  144,    2, 0x08 /* Private */,
+      22,    3,  145,    2, 0x08 /* Private */,
+      28,    1,  152,    2, 0x08 /* Private */,
+      29,    1,  155,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 23, 0x80000000 | 25, 0x80000000 | 25,   24,   26,   27,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3,    4,
+
+       0        // eod
+};
+
+void FileManager::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<FileManager *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->clickedFirst((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 1: _t->clickedSecond((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 2: _t->deleteItem(); break;
+        case 3: _t->copyItemFrom((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 4: _t->copyItemFrom(); break;
+        case 5: _t->copyItemTo((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 6: _t->copyItemTo(); break;
+        case 7: _t->openFile(); break;
+        case 8: _t->openHexFile(); break;
+        case 9: _t->renameItem(); break;
+        case 10: _t->runProgram(); break;
+        case 11: _t->searchFiles(); break;
+        case 12: _t->createDirectory(); break;
+        case 13: _t->createFile(); break;
+        case 14: _t->help(); break;
+        case 15: _t->unzipFiles(); break;
+        case 16: _t->zipFiles(); break;
+        case 17: _t->listArchive(); break;
+        case 18: _t->windowMessage((*reinterpret_cast< QFile(*)>(_a[1])),(*reinterpret_cast< const char*(*)>(_a[2])),(*reinterpret_cast< const char*(*)>(_a[3]))); break;
+        case 19: _t->on_tableView_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 20: _t->on_tableView_2_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject FileManager::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_FileManager.data,
+    qt_meta_data_FileManager,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *FileManager::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *FileManager::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_FileManager.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int FileManager::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 21;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
